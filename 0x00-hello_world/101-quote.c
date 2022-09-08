3fd437278d4222d9806c1bed50ad98475f0d2df1
mahelet <maheletdendir@gmail.com>
2022-09-08 07:12:50 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,9 +12,9 @@ int main(void)
 
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
 
 	return (1);
 
