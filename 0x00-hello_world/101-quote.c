678d88fd7af724df6e2ff242d69e898aa0d9d9d8
mahelet <maheletdendir@gmail.com>
2022-09-08 07:08:25 -0700

    Task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
 /**
  * main - Entry point
- *
+ * 
  * Return: Always 0 (Success)
  */
 
@@ -10,10 +12,10 @@ int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
-
-			return (1);
+	return (1);
 
 }
