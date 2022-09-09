caf6e340c7d589500f2c350c39bbac89c1fa98d4
mahelet <maheletdendir@gmail.com>
2022-09-09 12:42:30 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@
 * this program prints :Programming is positive, zero, or negative
 * Return: 0
 */
-int main (void)
+int main(void)
 {
 	int n;
-	for (n = '0'; n <= '9' ; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
