64e3fba364431be4f1af99dd8fa8cefce5e9903d
maheletd <maheletdendir@gmail.com>
2022-09-15 10:52:09 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * print_number - Prints a number
- * @n: The number to print
+ *print_number - Prints a number
+ *@n: The number to print
  */
 
 void print_number(int n)
 {
 	unsigned int num = n;
-	
+
 	if (n < 0)
 	{
 		putchar('-');
