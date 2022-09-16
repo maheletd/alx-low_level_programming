9f883d5fc5c62413bf91705e2cabbe2d140c3c9c
maheletd <maheletdendir@gmail.com>
2022-09-16 00:11:05 -0700

    Task 9

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,44 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- *print_number - Prints a number
- *@n: The number to print
+ * print_number -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
  */
+void print_number(int i
+	unsigned int z;
+			int m,
+				b = 10;
 
-void print_number(int n)
-{
-	unsigned int num = n;
+					if (n < 10 && n >= 0)
+							{
+										_putchar (n + '0');
+											}
+						else if (n > -10 && n < 0)
+								{
+											n = n - 2 * n;
+													_putchar('-');
+															_putchar (n + '0');
+																}
 
-	if (n < 0)
-	{
-		putchar('-');
-		num = -num;
-	}
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	putchar(num % 10 + '0');
+							else
+									{
+												if (n < 0)
+															{
+																			n = n * -1;
+																						_putchar ('-');
+																								}
+														z = n;
+															while (z / b > 9)
+																	{
+																				b = b * 10;
+																					}
+																while (b > 0)
+																		{
+																					m = z / b;
+																							z = z % b;
+																									_putchar (m + '0');
+																											b = b / 10;
+																												}
+																	}
 }
