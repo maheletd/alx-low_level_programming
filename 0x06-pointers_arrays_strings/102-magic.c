3939bc31dc6a2ab63c5c21e522c6cc1fb84a2cda
Mahelet <maheletdendir@gmail.com>
2022-09-22 06:23:22 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,6 +5,7 @@
  * Description: adds a line in the code and uses printf
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
@@ -13,6 +14,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -21,8 +23,11 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
+
 	p[5]  = 98;
+
 	/* ... so that this prints 98\n */
+
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
