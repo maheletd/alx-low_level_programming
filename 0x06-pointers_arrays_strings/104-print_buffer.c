5a0d11bb10648c8e81376b2ddd4e2fd6e11be8f5
Mahelet <maheletdendir@gmail.com>
2022-09-22 04:11:33 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,7 +3,7 @@
 
 /**
  * print_line - prints a s bytes of a buffer
-** @c: buffer to print
+ * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
  *
@@ -12,6 +12,7 @@
 void print_line(char *c, int s, int l)
 {
 	int j, k;
+	
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
@@ -40,6 +41,7 @@ void print_line(char *c, int s, int l)
 void print_buffer(char *b, int size)
 {
 	int i;
+
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
