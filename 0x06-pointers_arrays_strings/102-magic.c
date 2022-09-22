2407d4de7831dc543202c25d5db04944cc5d04d5
Mahelet <maheletdendir@gmail.com>
2022-09-22 06:00:51 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,6 +10,7 @@ int main(void)
 	int n;
 	int a[5];
 	int *p;
+
 	a[2] = 1024;
 	p = &n;
 	/*
@@ -23,4 +24,5 @@ int main(void)
 	p[5]  = 98;
 	/* ... so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	return (0);}
+	return (0);
+}
