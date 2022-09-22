7540772572e4c2941cd32a80a79c25d79b939df9
Mahelet <maheletdendir@gmail.com>
2022-09-22 05:53:20 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: adds a line in the code and uses printf
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -7,24 +12,15 @@ int main(void)
 	int *p;
 	a[2] = 1024;
 	p = &n;
-             	    /*
-		     *
-		     *    * write your line of code here...
-		     *
-		     *       * Remember:
-		     *
-		     *          * - you are not allowed to use a
-		     *
-		     *             * - you are not allowed to modify p
-		     *
-		     *                * - only one statement
-		     *
-		     *                   * - you are not allowed to code anything else than this line of code
-		     *
-		     *                      */
-
-		    *(p + 5) = 98;
-		      /* ...so that this prints 98\n */
-		      printf("a[2] = %d\n", a[2]);
-		        return (0);
-}
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	p[5]  = 98;
+	/* ... so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);}
