19d1d646d7fa849a0b9ed3a56422f108c65884ff
Mahelet <maheletdendir@gmail.com>
2022-10-24 07:06:15 -0700

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,6 +10,7 @@ void free_listp(listp_t **head)
 {
 	listp_t *temp;
 	listp_t *curr;
+
 	if (head != NULL)
 	{
 		curr = *head;
@@ -32,6 +33,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t nnodes = 0;
 	listp_t *hptr, *new, *add;
+
 	hptr = NULL;
 	while (head != NULL)
 	{
