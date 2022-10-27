8fe3d763b6f7c0e3d9b6df18097a6c52ce0c5549
Mahelet <maheletdendir@gmail.com>
2022-10-27 04:13:03 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
 	size_t n;
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
